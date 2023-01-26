d059f9618db375cefbde1d6ac4b52f79a60a5575
Zeyu Yao <novodoodle@gmail.com>
2023-01-26 20:52:04 +0800

    Separate header information

---
+++ components/Header.tsx
@@ -0,0 +1,65 @@
+import Head from "next/head";
+
+export default function Header() {
+  return (
+    <Head>
+      <title>Notify</title>
+      <meta
+        name="description"
+        content="Write a note for someone to sign or leave a message for someone to read."
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      {/* favicons */}
+      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+
+      {/* Open Graph */}
+      <meta property="og:title" content="Notify" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://notify.cytronicoder.com" />
+      <meta
+        property="og:description"
+        content="Write a note for someone to sign or leave a message for someone to read."
+      />
+      <meta
+        property="og:image"
+        content="https://notify.cytronicoder.com/og-image.png"
+      />
+      <meta property="og:image:alt" content="Notify logo" />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@cytronicoder" />
+      <meta name="twitter:creator" content="@cytronicoder" />
+      <meta name="twitter:title" content="Notify" />
+      <meta
+        name="twitter:description"
+        content="Write a note for someone to sign or leave a message for someone to read."
+      />
+      <meta
+        name="twitter:image"
+        content="https://notify.cytronicoder.com/og-image.png"
+      />
+      <meta name="twitter:image:alt" content="Notify logo" />
+      <meta name="twitter:url" content="https://notify.cytronicoder.com" />
+    </Head>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Image from "next/image";
 
 // Assets
@@ -6,6 +5,7 @@ import logo from "@/assets/logo.svg";
 import wordmark from "@/assets/wordmark.svg";
 
 // Components
+import Header from "@/components/Header";
 import Waitlist from "@/components/Waitlist";
 
 // Styles
@@ -14,35 +14,7 @@ import styles from "@/styles/Home.module.css";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Notify</title>
-        <meta
-          name="description"
-          content="Write a note for someone to sign or leave a message for someone to read."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        {/* favicons */}
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
+      <Header />
 
       <main className={styles.main}>
         <header className={styles.header}>
